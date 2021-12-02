b329c6de8655bf5dfc28f1175a1fee414b6d8a6d
Ehsan Jalali <JMSEhsan@gmail.com>
2021-12-01 23:30:03 -0700

    CS_Exercise_Class

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* ref. https://www.w3schools.com/cs/trycs.php?filename=demo_class
+*/
+
+// See https://aka.ms/new-console-template for more information
+
+using System;
+
+//namespace MyApplication
+//{
+  class Car
+  {
+    string color = "red";
+    string style = "sedan";
+    string year_made = "2020";
+
+    static void Main(string[] args)
+    {
+      Car myObj = new Car();
+      Console.WriteLine(myObj.color);
+      Console.WriteLine(myObj.style);
+      Console.WriteLine(myObj.year_made);
+    }
+  }
+//}
